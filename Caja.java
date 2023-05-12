06d2abedda0cce1e66d163ded80726d2ae458f49
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2023-05-12 08:59:57 -0500

    Se agregan las clases objeto

---
+++ Caja.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supermercadoLopez;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Caja {
+    private int idCaja;
+    private int totalFacturado;
+
+    public Caja() {
+    }
+
+    public Caja(int idCaja, int totalFacturado) {
+        this.idCaja = idCaja;
+        this.totalFacturado = totalFacturado;
+    }
+
+    public int getIdCaja() {
+        return idCaja;
+    }
+
+    public void setIdCaja(int idCaja) {
+        this.idCaja = idCaja;
+    }
+
+    public int getTotalFacturado() {
+        return totalFacturado;
+    }
+
+    public void setTotalFacturado(int totalFacturado) {
+        this.totalFacturado = totalFacturado;
+    }
+    
+    
+}
---
+++ Producto.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supermercadoLopez;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private int precio;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nombre, int precio) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
